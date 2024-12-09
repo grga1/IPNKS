5bbd9e8ee4ab65bda87511463b86dfd8a5ac86de
Filip Iliev <151552696+grga1@users.noreply.github.com>
2024-12-09 22:48:47 +0100

    Update KOD_ZA_ODGOVARANJE_ANKETI.js

--- KOD_ZA_ODGOVARANJE_ANKETI.js
+++ KOD_ZA_ODGOVARANJE_ANKETI.js
@@ -1,2 +1,3 @@
 //kod za brzo odgovaranje anketi, vnesi go samo vo consolata
+//code for answering surveys
 document.querySelectorAll('input[type="radio"][value="5"]').forEach(radio=>{  radio.click();});
